240f8a8cb5a64ca0fe3af646451023782ed9169f
mccanndt <mccanndt91@gmail.com>
2019-04-24 13:12:40 -0400

    Added and finished Table of Powers

---
+++ ch04_prj1_TableOfPowers/src/ch04_prj1_TableOfPowers/TableOfPowersApp.java
@@ -0,0 +1,38 @@
+package ch04_prj1_TableOfPowers;
+
+import java.util.Scanner;
+
+public class TableOfPowersApp {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to the Squares and Cubes table");
+		Scanner sc = new Scanner(System.in);
+
+		String choice = "y";
+		while (choice.equalsIgnoreCase("y")) {
+			// Get user input
+			System.out.print("Enter an integer: ");
+			int integer = sc.nextInt();
+
+			// Display results and math
+			System.out.println("Number\tSquared\tCubed");
+			System.out.println("======\t=======\t=====");
+
+			for (int i = 1; i <= integer; i++) {
+				// Math
+				int square = i * i;
+				int cube = i * i * i;
+				System.out.println(i + "\t" + square + "\t" + cube);
+			}
+
+			// Loop question
+			System.out.println("Continue? (y/n)");
+			choice = sc.next();
+			System.out.println();
+		}
+		System.out.println("Bye!");
+		sc.close();
+
+	}
+
+}
