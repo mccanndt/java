3824c921cf992f7dc6a2f5eadc4e769b3435c3b3
mccanndt <mccanndt91@gmail.com>
2019-04-26 15:41:53 -0400

    Added and finished Contact List App

---
+++ ch07_prj1_ContactList/src/ContactListApp.java
@@ -0,0 +1,24 @@
+
+public class ContactListApp {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to the Contact List application");
+		
+		String choice = "y";
+		while (choice.equalsIgnoreCase("y")) {
+			
+			String firstName = Console.getString("Enter first name: ");
+			String lastName = Console.getString("Enter last name: ");
+			String email = Console.getString("Enter email: ");
+			String phone = Console.getString("Enter phone: ");
+			
+			Contact contact = new Contact(firstName, lastName, email, phone);
+			
+			System.out.println(contact.displayContact());
+			// Loop?
+			choice = Console.getString("\nContinue? (y/n): ");
+		}
+
+	}
+
+}
