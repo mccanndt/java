493f20a26b85f30602351123725dc7a846ac7916
mccanndt <mccanndt91@gmail.com>
2019-05-14 11:40:19 -0400

    Added the Movie Ratings app

---
+++ ch22_prj1_MovieRatings/src/business/MovieCollection.java
@@ -0,0 +1,56 @@
+package business;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class MovieCollection {
+	private List<Movie> movies;
+	
+
+	public MovieCollection() {
+		movies = new ArrayList<>();
+	}
+
+	public void add(Movie movie) {
+		movies.add(movie);
+	}
+	
+	public List<Movie> getMovies() {
+		return movies;
+	}
+
+	public List<Movie> filterMovies(Predicate<Movie> condition) {
+		List<Movie> filteredMovies = new ArrayList<>();
+		for (Movie m : movies) {
+			if (condition.test(m)) {
+				filteredMovies.add(m);
+			}
+		}
+		return filteredMovies;
+	}
+
+	public double getLowestRating() {
+		return movies.stream()
+				.map(Movie::getRating)
+				.reduce(5.0, Math::min);
+	}
+
+	public double getHighestRating() {
+		return movies.stream()
+				.map(Movie::getRating)
+				.reduce(0.0, Math::max);
+	}
+
+	public double getAverageRating() {
+		double sum = movies.stream()
+				.map(Movie::getRating)
+				.reduce(0.0, (a, b) -> a + b);
+		return sum / getSize();
+	}
+	
+	public int getSize() {
+		return movies.size();
+	}
+
+}
